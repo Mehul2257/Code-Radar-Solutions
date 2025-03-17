2a79aff320b3f7aa2ea99c2547e2097d9f6378c9
Mehul Sharma <mehul1265.becse24@chitkara.edu.in>
2025-03-17 20:33:35 +0530

    Solution to Perform Bitwise OR

---
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("Enter the number");
+scanf("%d %d",&a,&b);
+
+if(a>0 OR b>0){
+    sum=a+b;
+    printf("%d",sum);
+}
+else
+printf("Number is invalid");
+
+return 0;
+}
+
